8b65134641c94019c27b7e261849bf7884ee5430
ideaTotal <30959803+ideatotalcom@users.noreply.github.com>
2023-11-27 17:53:52 -0800

    Persistencia

---
+++ Persistencia/Context/AplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using EstudiantesAPP.Dominio.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace EstudiantesAPP.Persistencia.Context
+{
+    public class AplicationDBContext:DbContext
+    {   
+        public DbSet<Estudiante> Estudiante { get; set; }
+
+        public AplicationDBContext(DbContextOptions<AplicationDBContext> options):base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder builder) 
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
