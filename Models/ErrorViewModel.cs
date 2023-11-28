24151de038cbfe73b35e7959b008b9253a9479d1
ideaTotal <30959803+ideatotalcom@users.noreply.github.com>
2023-11-27 17:52:34 -0800

    Modelos

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EstudiantesAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
