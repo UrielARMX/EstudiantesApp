b113baf01c0194f81fd77c8350f3a9086d145d62
ideaTotal <30959803+ideatotalcom@users.noreply.github.com>
2023-11-27 17:55:07 -0800

    Transporte

---
+++ Transporte/EstudianteDTO.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EstudiantesAPP.Transporte
+{
+    public class EstudianteDTO
+    {
+        
+        public int Id { get; set; }
+       
+        [Display(Name = "Nombre(s) del alumno")]
+
+        public string Nombres { get; set; }
+
+        [Display(Name = "Apellido(s) del alumno")]
+
+        public string Apellidos { get; set; }
+
+        [Display(Name = "Fecha de inscripción")]
+
+        public string FechaInscripcion { get; set; }
+
+        [Display(Name = "Fecha de inscripción")]
+        public DateTime FechaDate { get; internal set; }
+    }
+}
